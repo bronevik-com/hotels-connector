3d58d6bc593cd3683755d81b755391540e8fda2f
a.antropov <a.antropov@bronevik.com>
2019-07-12 15:26:57 +0500

    2.3.0 -> 2.2.1

--- src/HotelsConnector/Enum/Endpoints.php
+++ src/HotelsConnector/Enum/Endpoints.php
@@ -4,11 +4,11 @@ namespace Bronevik\HotelsConnector\Enum;
 
 abstract class Endpoints
 {
-    const PRODUCTION  = 'https://hotels-api.bronevik.com/v2.3.0/api.php';
-    const DEVELOPMENT = 'https://dev-hotels-api.bronevik.com/v2.3.0/api.php';
+    const PRODUCTION  = 'https://hotels-api.bronevik.com/v2.2.1/api.php';
+    const DEVELOPMENT = 'https://dev-hotels-api.bronevik.com/v2.2.1/api.php';
 
     public static $wsdlUrls = [
-        self::PRODUCTION  => 'https://hotels-api.bronevik.com/v2.3.0/api.wsdl',
-        self::DEVELOPMENT => 'https://dev-hotels-api.bronevik.com/v2.3.0/api.wsdl',
+        self::PRODUCTION  => 'https://hotels-api.bronevik.com/v2.2.1/api.wsdl',
+        self::DEVELOPMENT => 'https://dev-hotels-api.bronevik.com/v2.2.1/api.wsdl',
     ];
 }
