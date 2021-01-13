81c22e65531e427c273a6f3932a5b6db45ca91ac
d.domrachev <rwSkd67A>
2021-01-13 10:00:08 +0500

    #3855 заменил эндпойнты dev-hotel-api
    API / Доработки для переезда на новый dev
    https://gitlab.bronevik.space/bronevik/bronevik/-/issues/3855

--- src/HotelsConnector/Enum/Endpoints.php
+++ src/HotelsConnector/Enum/Endpoints.php
@@ -5,15 +5,15 @@ namespace Bronevik\HotelsConnector\Enum;
 abstract class Endpoints extends Enum
 {
     const PRODUCTION  = 'https://hotels-api.bronevik.com/v2.4.0/api.php';
-    const DEVELOPMENT = 'https://dev-hotels-api.bronevik.com/v2.4.0/api.php';
+    const DEVELOPMENT = 'https://hotels-api.dev.bronevik.tech/v2.4.0/api.php';
 
     const SECURE_PRODUCTION  = 'https://secure-hotels-api.bronevik.com/v2.4.0/api.php';
-    const SECURE_DEVELOPMENT = 'https://dev-secure-hotels-api.bronevik.com/v2.4.0/api.php';
+    const SECURE_DEVELOPMENT = 'https://secure-hotels-api.dev.bronevik.tech/v2.4.0/api.php';
 
     public static $wsdlUrls = [
         self::PRODUCTION         => 'https://hotels-api.bronevik.com/v2.4.0/api.wsdl',
-        self::DEVELOPMENT        => 'https://dev-hotels-api.bronevik.com/v2.4.0/api.wsdl',
+        self::DEVELOPMENT        => 'https://hotels-api.dev.bronevik.techv2.4.0/api.wsdl',
         self::SECURE_PRODUCTION  => 'https://hotels-api.bronevik.com/v2.4.0/secure-api.wsdl',
-        self::SECURE_DEVELOPMENT => 'https://dev-hotels-api.bronevik.com/v2.4.0/secure-api.wsdl',
+        self::SECURE_DEVELOPMENT => 'https://hotels-api.dev.bronevik.tech/v2.4.0/secure-api.wsdl',
     ];
 }
