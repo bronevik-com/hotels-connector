113ebb6350835aab187058bbb064e34809211050
Sergei Besedin <s.besedin@bronevik.com>
2023-03-01 13:09:07 +0700

    2.5.1

--- src/HotelsConnector/Enum/GuestsCounts.php
+++ src/HotelsConnector/Enum/GuestsCounts.php
@@ -4,6 +4,8 @@ namespace Bronevik\HotelsConnector\Enum;
 
 abstract class GuestsCounts extends Enum
 {
-    const ONE = 1;
-    const TWO = 2;
+    const ONE   = 1;
+    const TWO   = 2;
+    const THREE = 3;
+    const FOUR  = 4;
 }
