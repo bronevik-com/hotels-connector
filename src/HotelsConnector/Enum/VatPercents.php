adc74af8d8d17eda69e6ff138420422c78899b86
Alex Iskritsky <a.iskritsky@bronevik.com>
2024-04-09 14:46:41 +0500

    2.5.1 fix

--- src/HotelsConnector/Enum/VatPercents.php
+++ src/HotelsConnector/Enum/VatPercents.php
@@ -4,6 +4,7 @@ namespace Bronevik\HotelsConnector\Enum;
 
 abstract class VatPercents extends Enum
 {
+    const NDS_18      = 18;
     const NDS_20      = 20;
     const NDS_ZERO    = 1;
     const WITHOUT_NDS = 0;
